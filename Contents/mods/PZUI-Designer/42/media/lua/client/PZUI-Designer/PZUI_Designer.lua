091217ba8245d7aadbcc13421cb7552c1d099423
Jupiters-Workshop <jupitersworkshops@gmail.com>
2026-09-14 14:12:47 -0600

    Update PZUI_Designer.lua

--- Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/PZUI_Designer.lua
+++ Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/PZUI_Designer.lua
@@ -1,14 +1,35 @@
 --'PZUI Designer' By The Project Zomboid Modding Community (PZMC) 09132026
 
-local Options = PZAPI.ModOptions:create("HearingAidsOptions", "Hearing Aids Options")
+--libraries
+require "ISUI/ISPanel"
+
+---@class PZUI : ISPanel
+local PZUI = ISPanel:derive("PZUI-Designer/PZUI")
+local Options = PZAPI.ModOptions:create("PZUIOptions", "PZUI Designer Options")
 
 local KEY_TOGGLE_HA = Options:addKeyBind(
-    "Toggle_Hearing_Aids",
-    getText("HearingAid_HearingAidOptions_HearingAidKeybind"),
-    52
+    "Open Designer",
+    getText("Open Designer"),
+    68
 )
 
 
+local Designer
+Events.OnKeyPressed.Add(function(key)
+    -- verify the keybind is pressed
+    if key ~= Options:getOption("Open Designer"):getValue() then return end
 
+    -- if the UI exists, we close it
+    if Designer then
+        Designer:setVisible(false)
+        Designer:removeFromUIManager()
+        Designer = nil
 
-Events.OnKeyPressed.Add(OnKeyPressed)
+    -- else we create a new instance of that UI
+    else
+        Designer = PZUI:new(0, 0, getCore():getScreenWidth(), getCore():getScreenHeight())
+        Designer:initialise()
+        Designer:addToUIManager()
+    end
+end)
+
