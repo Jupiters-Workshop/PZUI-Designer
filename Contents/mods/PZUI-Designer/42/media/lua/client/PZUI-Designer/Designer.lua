d4b5a97405898d7aec8d260796d39415a6092257
Jupiters-Workshop <jupitersworkshops@gmail.com>
2026-09-15 13:30:59 -0600

    Update

--- Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/Designer.lua
+++ Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/Designer.lua
@@ -6,6 +6,38 @@ require "ISUI/ISPanel"
 ---@class PZUI : ISPanel
 local PZUI = ISPanel:derive("PZUI-Designer/PZUI")
 
+--Initialize Useful Variables
+local Margin = 25 
+
+function PZUI:initialise()
+    ISPanel.initialise(self)
+    self:create()
+end
+
+function PZUI:prerender()
+    ISPanel.prerender(self)
+end
+
+function PZUI:render()
+
+end
+
+function PZUI:create() --our Custom create function that sets the Designer Tools look correctly
+    --Our Designer Interface
+    local ci_h = self.height - Margin * 2
+    local ci_w = ci_h * 0.5
+    local cInterface = ISPanel:new(Margin,Margin,ci_w,ci_h)
+    cInterface.backgroundColor = {r=0, g=0, b=0, a=1}
+    self:addChild(cInterface)
+
+end
+
+function PZUI:new(x, y, width, height)
+    local o = ISPanel.new(self, x, y, width, height)
+    return o
+end
+
+return PZUI
 
 
 
--- Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/PZUI_Handler.lua
+++ Contents/mods/PZUI-Designer/42/media/lua/client/PZUI-Designer/PZUI_Handler.lua
@@ -1,36 +1,29 @@
 --'PZUI Handler' By The Project Zomboid Modding Community (PZMC) 09152026
 
-local Designer = require 'PZUI-Designer/Designer'
+local PZUI = require 'PZUI-Designer/Designer'
 local Options = PZAPI.ModOptions:create("PZUIOptions", "PZUI Designer Options")
 
-local KEY_TOGGLE_HA = Options:addKeyBind(
+local _KEY_TOGGLE_HA = Options:addKeyBind(
     "Open Designer",
     getText("Open Designer"),
     68
 )
 
+local Designer
 Events.OnKeyPressed.Add(function(key)
     -- verify the keybind or escape is pressed
-    if key ~= Options:getOption("Open Designer"):getValue() and key ~= 1 then return end
+    if key ~= Options:getOption("Open Designer"):getValue() then return end
 
     -- if the UI exists, we close it
-    if Designer and key == 1 then
-        UIManager.getSpeedControls():SetCurrentGameSpeed(1)
+    if Designer then
         Designer:setVisible(false)
         Designer:removeFromUIManager()
         Designer = nil
 
-    elseif Designer and key ~= 1 then --if our keybind was pressed instead we still close it
-        UIManager.getSpeedControls():SetCurrentGameSpeed(1)
-        Designer:setVisible(false)
-        Designer:removeFromUIManager()
-        Designer = nil
-
-    elseif key == Options:getOption("Open Designer"):getValue() then -- else we create a new instance of that UI only when our keybind is pressed
+    else -- else we create a new instance of that UI
         UIManager.getSpeedControls():SetCurrentGameSpeed(0)
         Designer = PZUI:new(0, 0, getCore():getScreenWidth(), getCore():getScreenHeight())
-        Designer.backgroundColor = {r=1, g=1, b=1, a=1}
-        Designer.borderColor = {r=0, g=0, b=0, a=1}
+        Designer.backgroundColor = {r=0.0969, g=0.0969, b=0.0969, a=1}
         Designer:initialise()
         Designer:addToUIManager()
     end
